d1d89e6242d4901e32ef92a1d359ba0e0022ca99
JoltiPranik <tnt5905@gmail.com>
2026-04-14 19:47:19 +0300

    add itter2 full

--- Godot_games/Cutted fantasy/новый-игровой-проект/Scene/enemy.gd
+++ Godot_games/Cutted fantasy/новый-игровой-проект/Scene/enemy.gd
@@ -1,17 +1,45 @@
 extends CharacterBody2D
 @onready var Player = $"../Player"
-var speed = 100
+@onready var attack = preload("res://Scene/Mob_Attack.tscn")
+var speed = 25
 var trigger_move = false
+var attack_trigger = false
+var health = 10
 
 func _process(delta: float) -> void:
-	var direction = (Player.position - self.position).normalized()
+	var direction = (Player.position - $".".position).normalized()
 	if trigger_move == true:
 		velocity = direction * speed
 	else:
 		velocity = Vector2.ZERO
 	set_velocity(velocity)
 	move_and_slide()
+	attack_melee()
+	
+	if health <= 0:
+		queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == 'Player':
 		trigger_move = true
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.name == 'Player':
+		attack_trigger = true
+
+func attack_melee():
+	if attack_trigger == true:
+		var udar = attack.instantiate()
+		var deltax = Player.position.x - $".".position.x
+		var deltay = Player.position.y - $".".position.y
+		var lenght = sqrt(deltax * deltax + deltay * deltay)
+		udar.position.x += 25 * deltax / lenght
+		udar.position.y += 30 * deltay / lenght
+		udar.rotation -= udar.position.angle_to($".".position)
+		add_child(udar)
+		await get_tree().create_timer(0.05).timeout
+		udar.free()
+
+		
+func damage(input_damage: int):
+	health -= input_damage
--- Godot_games/Cutted fantasy/новый-игровой-проект/Script/Player.gd
+++ Godot_games/Cutted fantasy/новый-игровой-проект/Script/Player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
-
+@onready var attack = preload("res://Scene/Player_Attack.tscn")
 var speed = 500
+var  trigger = false
+var health = 100
 
 func _process(delta: float) -> void:
 	var direction = Vector2.ZERO
+	if velocity != Vector2.ZERO:
+		trigger = true
+		$AnimatedSprite2D.play("Walk")
+	else:
+		trigger = false
+		$AnimatedSprite2D.stop()
 	
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 10
@@ -16,8 +24,14 @@ func _process(delta: float) -> void:
 
 	set_velocity(direction * speed * delta)
 	move_and_slide()
-
-	if velocity != Vector2.ZERO:
-		$AnimatedSprite2D.play("Walk")
-	else:
-		$AnimatedSprite2D.stop()
+	if health <= 0:
+		queue_free()
+		
+	if Input.is_action_pressed("ui_accept") and trigger == true:
+		var udar = attack.instantiate()
+		udar.global_position = direction + direction
+		add_child(udar)
+		await get_tree().create_timer(0.05).timeout
+		udar.free()
+func damage(input_damage: int):
+	health -= input_damage
